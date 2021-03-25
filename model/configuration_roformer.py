bf2a302813b38607ad3b96530c09a2dc90b0be11
junnyu <573009727@qq.com>
2021-03-25 15:21:32 +0800

    upate

--- model/configuration_roformer.py
+++ model/configuration_roformer.py
@@ -90,7 +90,6 @@ class RoFormerConfig(PretrainedConfig):
                  hidden_act="gelu",
                  hidden_dropout_prob=0.1,
                  attention_probs_dropout_prob=0.1,
-                 max_position_embeddings=512,
                  type_vocab_size=2,
                  initializer_range=0.02,
                  layer_norm_eps=1e-12,
@@ -109,7 +108,6 @@ class RoFormerConfig(PretrainedConfig):
         self.intermediate_size = intermediate_size
         self.hidden_dropout_prob = hidden_dropout_prob
         self.attention_probs_dropout_prob = attention_probs_dropout_prob
-        self.max_position_embeddings = max_position_embeddings
 
         self.type_vocab_size = type_vocab_size
         self.initializer_range = initializer_range
